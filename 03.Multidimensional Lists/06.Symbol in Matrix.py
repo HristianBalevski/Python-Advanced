be35079dfbb1a81994b78a533feb55bc4818651d
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-21 11:54:06 +0000

    Homework Lab

--- 03.Multidimensional Lists/06.Symbol in Matrix.py
+++ 03.Multidimensional Lists/06.Symbol in Matrix.py
@@ -6,16 +6,16 @@ for info in range(size_of_matrix):
     matrix.append(symbols)
 
 searched_symbol = input()
-symbol_is_find = False
+symbol_is_found = False
 
 for row in range(size_of_matrix):
     for col in range(size_of_matrix):
         if matrix[row][col] == searched_symbol:
-            symbol_is_find = True
+            symbol_is_found = True
             print(f"({row}, {col})")
             break
-    if symbol_is_find:
+    if symbol_is_found:
         break
 
-if not symbol_is_find:
+if not symbol_is_found:
     print(f'{searched_symbol} does not occur in the matrix')
