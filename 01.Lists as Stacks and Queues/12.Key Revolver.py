93da4d09f44cc04585fa5bfc01e98b3d5d5895ba
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2023-01-14 16:05:44 +0000

    Homework Exercise

--- 01.Lists as Stacks and Queues/12.Key Revolver.py
+++ 01.Lists as Stacks and Queues/12.Key Revolver.py
@@ -1,37 +1,35 @@
 from collections import deque
 
-price_of_bullet = int(input())
-size_of_the_gun_barrel = int(input())
+bullet_price = int(input())
+size_of_gun_barrel = int(input())
 bullets = [int(num) for num in input().split()]
 locks = deque([int(num) for num in input().split()])
-value_of_the_intelligence = int(input())
+intelligence = int(input())
 
-shot_counter = 0
-barrel_counter = 0
+shoot_counter = 0
+shooting = 0
 
 while True:
     if len(bullets) == 0 or len(locks) == 0:
         break
-    current_bullet = bullets[-1]
-    current_lock = locks[0]
 
-    if current_bullet <= current_lock:
-        print('Bang!')
-        locks.popleft()
-        bullets.pop()
-        shot_counter += 1
-        barrel_counter += 1
+    current_bullet = bullets.pop()
+    current_lock = locks.popleft()
+
+    if current_lock >= current_bullet:
+        print("Bang!")
     else:
-        shot_counter += 1
-        barrel_counter += 1
-        bullets.pop()
+        locks.appendleft(current_lock)
         print("Ping!")
-        
-    if len(bullets) > 0 and barrel_counter == size_of_the_gun_barrel:
-        print("Reloading!")
-        barrel_counter = 0
 
-if len(bullets) >= 0 and len(locks) == 0:
-    print(f"{len(bullets)} bullets left. Earned ${value_of_the_intelligence - shot_counter * price_of_bullet}")
+    shooting += 1
+    shoot_counter += 1
+
+    if shooting == size_of_gun_barrel and bullets:
+        shooting = 0
+        print("Reloading!")
+
+if not locks:
+    print(f"{len(bullets)} bullets left. Earned ${intelligence - shoot_counter * bullet_price}")
 else:
     print(f"Couldn't get through. Locks left: {len(locks)}")
